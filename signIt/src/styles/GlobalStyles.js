5972fa8654fc1d132a3ad30d98093df48ed765df
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-03-28 02:06:30 +0200

    good styles

--- signIt/src/styles/GlobalStyles.js
+++ signIt/src/styles/GlobalStyles.js
@@ -1,33 +1,35 @@
-import { GlobalStyles as MuiGlobalStyles } from '@mui/material';
+// import { Global as MuiGlobalStyles } from '@mui/material';
 
-const GlobalStyles = () => (
-  <MuiGlobalStyles
-    styles={{
-      '*': {
-        margin: 0,
-        padding: 0,
-        boxSizing: 'border-box',
-      },
-      html: {
-        WebkitFontSmoothing: 'antialiased',
-        MozOsxFontSmoothing: 'grayscale',
-        height: '100%',
-        width: '100%',
-      },
-      body: {
-        height: '100%',
-        width: '100%',
-      },
-      '#root': {
-        height: '100%',
-        width: '100%',
-      },
-      a: {
-        textDecoration: 'none',
-        color: 'inherit',
-      },
-    }}
-  />
-);
+// const GlobalStyles = () => (
+//   <MuiGlobalStyles
+//     styles={{
+//       '*': {
+//         margin: 0,
+//         padding: 0,
+//         boxSizing: 'border-box',
+//       },
+//       html: {
+//         WebkitFontSmoothing: 'antialiased',
+//         MozOsxFontSmoothing: 'grayscale',
+//         height: '100%',
+//         width: '100%',
+//       },
+//       body: {
+//         height: '100%',
+//         width: '100%',
+//       },
+//       '#root': {
+//         height: '100%',
+//         width: '100%',
 
-export default GlobalStyles;
+        
+//       },
+//       a: {
+//         textDecoration: 'none',
+//         color: 'inherit',
+//       },
+//     }}
+//   />
+// );
+
+// export default GlobalStyles;
